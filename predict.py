ae9d52a3b1e4a911e609e0a012e6d10b0007e33b
Martin Savko <savko@synchrotron-soleil.fr>
2022-12-21 20:04:49 +0100

    predict.py    make port number a command line option

--- predict.py
+++ predict.py
@@ -31,7 +31,8 @@ if __name__ == '__main__':
     parser.add_argument('-H', '--prediction_heigth', default=256, type=int, help='prediction_heigth')
     parser.add_argument('-W', '--prediction_width', default=320, type=int, help='prediction_width')
     parser.add_argument('-s', '--save', default=1, type=int, help='save')
-    parser.add_argument('-p', '--prefix', type=str, default='test', help='prefix')
+    parser.add_argument('-P', '--prefix', type=str, default='test', help='prefix')
+    parser.add_argument('-p', '--port', type=int, default=8099, help='port')
     args = parser.parse_args()
     
     print('args', args)
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     request_arguments['model_img_size'] = model_img_size
     request_arguments['save'] = bool(args.save)
     request_arguments['prefix'] = args.prefix
-    predictions = get_predictions(request_arguments)
+    predictions = get_predictions(request_arguments, port=args.port)
     
     print('Client got all predictions')
     print('Most likely click: (vertical %.2f, horizontal %.2f)' % (get_most_likely_click(predictions)))
