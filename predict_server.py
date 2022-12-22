663aca6d9f5997d23eaf307db685d4d6f009d193
Martin Savko <savko@synchrotron-soleil.fr>
2022-12-22 15:13:14 +0100

    Add warmup server action. Addressing the fact that first predicition takes much longer then the subsequent ones. Both on CPU and GPU.

--- predict_server.py
+++ predict_server.py
@@ -3,6 +3,7 @@
 # author: Martin Savko (martin.savko@synchrotron-soleil.fr)
 
 import os
+import h5py
 import zmq
 import time
 import sys
@@ -15,14 +16,22 @@ from murko import predict_multihead, get_uncompiled_tiramisu
 
 
 def get_model(model_name='model.h5'):
-    _start = time.time()
+    _start_load = time.time()
     for gpu in tf.config.list_physical_devices('GPU'): 
         print('setting memory_growth on', gpu)
         tf.config.experimental.set_memory_growth(gpu, True)
     model = get_uncompiled_tiramisu()
     model.load_weights(model_name)
-    _end = time.time()
-    print('model loaded in %.4f seconds' % (_end-_start))
+    _end_load = time.time()
+    print('model loaded in %.4f seconds' % (_end_load-_start_load))
+    _start_warmup = time.time()
+    m = h5py.File(model_name, 'r')
+    warmup_image = m['warmup_image'][()][0]
+    m.close()
+    all_predictions = predict_multihead(to_predict=[warmup_image.tobytes()], model_img_size=(256, 320), model=model, save=False)
+    _end_warmup = time.time()
+    print('server warmup run took %.4f seconds' % (_end_warmup - _start_warmup))
+    
     return model
 
 def serve(port=8099, model_name='model.h5'):
