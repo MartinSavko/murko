d21a55b63da42e5a764b2a105bcdd014a01c3a4c
Kate Smith <katemarylouisesmith@gmail.com>
2024-05-01 08:36:16 +0100

    initial redis client

---
+++ redis_client.py
@@ -0,0 +1,56 @@
+import redis
+import threading
+
+class RedisClient:
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
+        self.pubsub = self.redis_client.pubsub()
+        self.subscribed_channels = set()
+        self.pubsub_thread = threading.Thread(target=self._listen_for_messages)
+        self.pubsub_thread.daemon = True
+        self.pubsub_thread.start()
+
+    def _listen_for_messages(self):
+        for message in self.pubsub.listen():
+            if message['type'] == 'message':
+                print(f"Received message: {message['data'].decode('utf-8')}")
+
+    def set(self, key, value):
+        self.redis_client.set(key, value)
+
+    def get(self, key):
+        return self.redis_client.get(key)
+
+    def publish(self, channel, message):
+        self.redis_client.publish(channel, message)
+
+    def subscribe(self, channel):
+        if channel not in self.subscribed_channels:
+            self.pubsub.subscribe(channel)
+            self.subscribed_channels.add(channel)
+            print(f"Subscribed to channel: {channel}")
+        else:
+            print(f"Already subscribed to channel: {channel}")
+
+    def unsubscribe(self, channel):
+        if channel in self.subscribed_channels:
+            self.pubsub.unsubscribe(channel)
+            self.subscribed_channels.remove(channel)
+            print(f"Unsubscribed from channel: {channel}")
+        else:
+            print(f"Not subscribed to channel: {channel}")
+
+    def close(self):
+        self.redis_client.close()
+
+
+if __name__ == "__main__":
+    client = RedisClient()
+
+    # Set and get example
+    client.set('metadata', 'test')
+    print(client.get('metadata'))
+
+    # Publish/Subscribe example
+    client.subscribe('murko')
+    client.publish('murko', 'metadata available!')
