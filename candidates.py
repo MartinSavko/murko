00b277b65f2d4b2edd905344ecd7bf6e6bd47d46
Martin Savko <savko@synchrotron-soleil.fr>
2025-06-17 15:35:20 +0200

    generate candidate learning tasks

---
+++ candidates.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+
+a = """How to represent and learn points ?
+    I see two options: 
+      a) learn offset(s) from point for each learning location i.e. have two location maps, one for horizontal offset and one for vertical offset. 
+          1.) have separate head for each of the point categories. i.e. "loop_inner_point" would be learned separately from "crystal_inner_point". ( But what if we have more then one crystal in the image?)
+          2.) each category of points has one learing output i.e. "inner_centers"
+           each learning location than learns offset to the nearest inner point.
+      b) learn 2d representation of point locations, 
+          1.) a supperposition of gaussians for "inner_centers", "centroids", "bbox_centers". 
+              What about "extreme_points", "eigen_points" and "global_keypoints"? *) Each subcategory e.g. "extreme_point_topmost" or "extreme" or "start_likely" has a separate output.
+              **) "extreme_points" has one output (four peak gaussian mixture)
+                  "start_likely" has one output (one gaussian)
+          2.) offset learners they learn x, y distance to the nearest keypoint
+    
+    What should offset learners do if there is no point of interest present?
+      a) do nothing ?
+      b) learn to report there is nothing by returning something specific e.g. -1 ?
+    
+    Learning bbox_centers? (centerness)
+             inner_centers?"""
+
+
+def get_candidates(
+    concepts=[
+        "crystal",
+        "area_of_interest",
+        "loop_inside",
+        "loop",
+        "stem",
+        "pin",
+        "ice",
+        "capillary",
+        "foreground",
+        "aether",
+        "explorable",
+        "support",
+        "drop",
+        "diffracting_area",  # from raster scans
+    ],
+    categorical={
+        "hierarchy_detailed": [
+            "crystal",
+            "loop_inside",
+            "loop",
+            "stem",
+            "pin",
+            "foreground",
+            "background",
+        ],
+        "hierarchy_crystal_aoi_support_pin": [
+            "crystal",
+            "area_of_interest",
+            "support",
+            "pin",
+            "foreground",
+            "background",
+        ],
+        "hierarchy_aoi": ["area_of_interest", "foreground", "background"],
+        "hierarchy_crystal": ["crystal", "foreground", "background"],
+    },
+    encoders=["identity", "identity_bw"],
+    keypoints=[
+        "most_likely_click",
+        "extreme",
+        "end_likely",
+        "start_likely",
+        "start_possible",
+        "origin",
+    ],
+    classifications={
+        "anything": ["foreground"],
+        "plate_content": ["crystals", "precipitate", "other", "clear"],
+        "ice": ["ice"],
+        "loop_type": ["standard", "mitegen", "crystal_direct", "void"],
+    },
+    retionprops=["area", "perimeter", "aspect", "extent", "solidity"],
+    parameters={
+        "binary_segmentation": {"channels": 1, "dtype": "int8"},
+        "distance_transform": {"channels": 1, "dtype": "float32"},
+        "inverse_distance_transform": {"channels": 1, "dtype": "float32"},
+        "sqrt_distance_transform": {"channels": 1, "dtype": "float32"},
+        "sqrt_inverse_distance_transform": {"channels": 1, "dtype": "float32"},
+        "centerness": {"channels": 1, "dtype": "float32"},
+        # ltrb + centerness
+        "bounding_box": {"channels": 1 + 4, "dtype": "float32"},
+        "min_rectangle": {"channels": 5, "dtype": "float32"},
+        "ellipse": {"channels": 5, "dtype": "float32"},
+        "moments": {"channels": 9, "dtype": "float32"},
+        "regionprops": {"channels": None, "dtype": "float32"},
+        "inner_center": {"channels": 1 + 2, "dtype": "float32"},
+        "centroid": {"channels": 1 + 2, "dtype": "float32"},
+        "bbox_center": {"channels": 1 + 2, "dtype": "float32"},
+        "extreme_points": {"channels": 1 + 8, "dtype": "float32"},
+        "eigen_points": {"channels": 1 + 8, "dtype": "float32"},
+        "encoder": {"channels": None, "dtype": "float32"},
+        "categorical_segmentation": {"channels": None, "dtype": "float32"},
+        # may make it a variable + centerness ?
+        "encoded_shape": {"channels": 21, "dtype": "float32"},
+        "keypoint": {"channels": 1 + 2, "dtype": "float32"},
+        "classification": {"channels": None, "dtype": "int8"},
+    },
+):
+
+    binary_segmentation_concepts = copy.copy(concepts)
+
+    distance_transform_concepts = copy.copy(concepts)
+    for concept in ["ice", "diffracting_area"]:
+        del distance_transform_concepts[distance_transform_concepts.index(concept)]
+
+    bounding_box_concepts = copy.copy(concepts)
+    for concept in ["ice", "diffracting_area", "aether"]:
+        del bounding_box_concepts[bounding_box_concepts.index(concept)]
+
+    encoded_shape_concepts = copy.copy(concepts)
+    for concept in [
+        "ice",
+        "diffracting_area",
+        "aether",
+        "support",
+        "explorable",
+        "foreground",
+    ]:
+        del encoded_shape_concepts[encoded_shape_concepts.index(concept)]
+
+    classes_of_tasks = {
+        # segmentation
+        "binary_segmentation": binary_segmentation_concepts,
+        "distance_transform": distance_transform_concepts,
+        "inverse_distance_transform": distance_transform_concepts,
+        "sqrt_distance_transform": distance_transform_concepts,
+        "sqrt_inverse_distance_transform": distance_transform_concepts,
+        # regressions
+        "bounding_box": bounding_box_concepts,
+        # learn four layer output (ltrb) separately,
+        # learn associated centerness separately or on the same branch ?
+        # centerness vs. inner center ?
+        "min_rectangle": bounding_box_concepts,
+        "moments": bounding_box_concepts,
+        "ellipse": bounding_box_concepts,
+        "regionprops": bounding_box_concepts,
+        "inner_center": bounding_box_concepts,
+        # modified centerness, offsets, heatmap, distance, (1 - distance), sqrt(1-distance),
+        # for point p
+        # xv, yv = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
+        # d = np.sqrt((p[0]-yv)**2 + (p[1]-xv)**2)
+        # d = d / d.max()
+        # d = (1 - d)**2
+        # if p not present d = -1
+        "centerness": bounding_box_concepts,
+        # binary cross entropy, or focal loss
+        # modified centerness d = (1 - centerness**2)**2
+        "extreme_points": bounding_box_concepts,
+        # heatmap for every class of objects and every type of point
+        # + offset to the center_of_mass (x, y, 2 layers)
+        # + size of the object (width and height, 2 layers)
+        # + area of the object (1 layer)
+        "eigen_points": bounding_box_concepts,
+        # heatmap for every class of objects and every type of point
+        # + major_axis, minor_axis (2 layers)
+        # + offset to the center of ellipse (x, y, 2 layers)
+        # + orientation (8 layers) according to Mousavian, or a single number?
+        # + area of the object (1 layer)
+        # + euler number (1 layer)
+        # + solidity (1 layer)
+        "encoded_shape": encoded_shape_concepts,
+        # C (e.g. C=20) layer output
+    }
+
+    candidates = []
+
+    for cot in classes_of_tasks:
+        for concept in classes_of_tasks[cot]:
+            task = {
+                "name": concept,
+                "task": cot,
+                "dtype": parameters[cot]["dtype"],
+                "channels": parameters[cot]["channels"],
+            }
+            candidates.append(task)
+
+    cot = "categorical_segmentation"
+    for hierarchy, concepts in categorical.items():
+        task = {
+            "name": hierarchy,
+            "task": cot,
+            "dtype": parameters[cot]["dtype"],
+            "channels": len(concepts),
+            "concepts": concepts,
+        }
+        candidates.append(task)
+
+    cot = "encoder"
+    for concept in encoders:
+        task = {
+            "name": concept,
+            "task": cot,
+            "dtype": parameters[cot]["dtype"],
+            "channels": 1 if "bw" in concept else 3,
+        }
+        candidates.append(task)
+
+    cot = "keypoint"
+    for concept in keypoints:
+        task = {
+            "name": concept,
+            "task": cot,
+            "dtype": parameters[cot]["dtype"],
+            "channels": parameters[cot]["channels"],
+        }
+        candidates.append(task)
+        # heatmap for every type of point
+
+    cot = "classification"
+    for classification, concepts in classifications.items():
+        task = {
+            "name": classification,
+            "task": cot,
+            "dtype": parameters[cot]["dtype"],
+            "channels": len(concepts),
+            "concepts": concepts,
+        }
+        candidates.append(task)
+
+    return candidates
