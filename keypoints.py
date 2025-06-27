0d875703264833b0406ac9e191674a3eb4d188bc
Martin Savko <savko@synchrotron-soleil.fr>
2025-06-27 19:50:14 +0200

    Move more keypoint related methods to keyopints.py

--- keypoints.py
+++ keypoints.py
@@ -7,6 +7,37 @@ import numpy as np
 from scipy.spatial import distance_matrix
 import peakutils
 
+
+def principal_axes(array, verbose=False):
+    # https://github.com/pierrepo/principal_axes/blob/master/principal_axes.py
+    _start = time.time()
+    if array.shape[1] != 3:
+        xyz = np.argwhere(array == 1)
+    else:
+        xyz = array[:, :]
+
+    coord = np.array(xyz, float)
+    center = np.mean(coord, 0)
+    coord = coord - center
+    inertia = np.dot(coord.transpose(), coord)
+    e_values, e_vectors = np.linalg.eig(inertia)
+    order = np.argsort(e_values)[::-1]
+    eigenvalues = np.array(e_values[order])
+    eigenvectors = np.array(e_vectors[:, order])
+    _end = time.time()
+    if verbose:
+        print("principal axes")
+        print("intertia tensor")
+        print(inertia)
+        print("eigenvalues")
+        print(eigenvalues)
+        print("eigenvectors")
+        print(eigenvectors)
+        print("principal_axes calculated in %.4f seconds" % (_end - _start))
+        print()
+    return xyz, inertia, eigenvalues, eigenvectors, center
+
+
 def get_origin(labels, indices, points, properties):
     origin = (-1, -1)
     if "foreground" in labels:
@@ -125,7 +156,9 @@ def get_aoi_keypoints(labels, indices, points, properties, label="area_of_intere
     return ab, at, al, ar
 
 
-def get_pin_right_and_left(labels, indices, points, properties, min_dist=0.25, filter_window=11):
+def get_pin_right_and_left(
+    labels, indices, points, properties, min_dist=0.25, filter_window=11
+):
     r = (-1, -1)
     l = (-1, -1)
     if "pin" in labels:
@@ -146,7 +179,7 @@ def get_pin_right_and_left(labels, indices, points, properties, min_dist=0.25, f
             pin - sp, axis=1, ord=2
         )
 
-        indices = peakutils.indexes(-(d2-d1), min_dist=0.25*len(pin), thres=0.5)
+        indices = peakutils.indexes(-(d2 - d1), min_dist=0.25 * len(pin), thres=0.5)
         print(f"indices {indices}")
         p1 = pin[indices[0]]
         p2 = pin[indices[1]]
@@ -193,3 +226,82 @@ def draw_point(point, ax=None, radius=3, color="red"):
 
     p = pylab.Circle(point, radius=radius, color=color)
     ax.add_patch(p)
+
+
+def get_orientation_and_direction(origin, extreme):
+    # assuming origin and extreme points are in [V, H] format
+    vector = global_extreme - global_origin
+    theta = np.degrees(np.arctan2(vector[0], vector[1]))
+    if (theta <= 45.0 and theta > -45.0) or (theta >= 135 and theta < -135):
+        orientation = 1
+    else:
+        orientation = 0
+    if vector[orientation] > 0:
+        direction = +1
+    else:
+        direction = -1
+    return orientation, direction
+
+
+def get_oriented_unit_cross(
+    orientation,  # 0 or 1; 0 for vertical, 1 for horizontal
+    direction,  # 1 or -1; 1 for rising pixel number, -1 for decreasing pixel number
+    unit_cross=[[1.0, 0.0], [-1.0, 0.0], [0.0, -1.0], [0.0, 1.0]],
+):
+
+    ouc = np.array(unit_cross)
+    if orientation == 1:
+        ouc = ouc[:, ::-1]
+
+    if direction != 0:
+        ouc[ouc != 0] = ouc[ouc != 0] * direction
+
+    return ouc
+
+
+def get_minmax(projection, atol=5):
+
+    xyz, inertia, e, S, center = principal_axes(projection)
+
+    S_inv = np.linalg.inv(S)
+    xyz_O = xyz - center
+    xyz_S = np.dot(xyz_O, S)
+
+    minmax = {}
+    for a, k in zip(("major", "minor"), (0, 1)):
+        xyz_S_onaxis = xyz_S[np.isclose(xyz_S[:, abs(k - 1)], 0.0, atol=atol)]
+
+        for l in ("min", "max"):
+            key = f"{l}_{a}"
+            try:
+                minmax[key] = xyz_S_onaxis[getattr(np, f"arg{l}")(xyz_S_onaxis[:, k])]
+            except:
+                minmax[key] = xyz_S[getattr(np, f"arg{l}")(xyz_S[:, k])]
+
+            minmax[key] = np.dot(minmax[key], S_inv) + center
+
+    return minmax
+
+
+def get_named_pca_points(
+    projection,
+    orientation,
+    direction,
+    atol=5,
+    default=np.array((-1, -1)),
+    order=["top", "bottom", "left", "right"],
+):
+
+    minmax = get_minmax(projection, atol=atol)
+
+    ouc = get_oriented_unit_cross(orientation, direction)
+    points = [item[key] for key in minmax]
+    scaled = [a / np.linalg.norm(a) for a in points]
+    dm = distance_matrix(ouc, scaled)
+    point_order = np.argmin(dm, axis=1)
+
+    npp = {"center": center}
+    for k, name in enumerate(order):
+        npp[name] = points[point_order[k]]
+
+    return npp
